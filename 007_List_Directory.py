f37e7355b6e77873080c47feb92dcc3e8c3fc1f0
George Chan <ygchan89@gmail.com>
2019-06-06 22:36:53 -0400

    Add a file

---
+++ 007_List_Directory.py
@@ -0,0 +1,24 @@
+# Reference: https://automatetheboringstuff.com/chapter8/
+# A program that list the filename in the current directory
+
+import os
+
+# Print the list of filename in our directory
+currentDir = os.getcwd()
+filename_list = os.listdir(currentDir)
+
+# Sorting it to look pretty.
+filename_list.sort()
+
+# Print out what the list contain
+print(filename_list)
+print(str(type(filename_list)) + '\n') # A list!
+
+# Loop each file and print it out
+for filename in filename_list:
+    # If the file is a python source file
+    if filename.endswith('.py'):
+        # Use path.join to apply proper forward slashes
+        myString = os.path.join(currentDir, filename)
+        # Display it to the user
+        print(myString)
