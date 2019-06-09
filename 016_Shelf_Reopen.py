92f39fa6f152edc32fc974cd4963fad1530e7f79
George Chan <ygchan89@gmail.com>
2019-06-09 09:42:37 -0400

    Improve documentation

--- 016_Shelf_Reopen.py
+++ 016_Shelf_Reopen.py
@@ -15,5 +15,27 @@ print(shelfFile)
 # But you can access the list.
 print(shelfFile['cats'])
 
+# Similiar to a dictionary, shelve is like a dictionary
+# it has keys() and values()
+
+print(list(shelfFile.keys()))
+print(list(shelfFile.values()))
+
 # Good habit to close it
 shelfFile.close()
+
+# Plaintext is useful for creating a data extract, but if you want to save
+# "data" from a program run, or maybe like a "state" of your program.
+# Then shelve module is an excellent tool. :)
+
+# Recap: import shelve to allow using shelve module
+# You can save an object using
+# shelfFile = shelve.open('myData') << This create an binary shelve file
+# shelfFile['name'] = object        << object is what you want to store
+
+# if you want to load it back
+# shelfFile = shelve.open('myData')
+# object = shelFile['name']         << This gives back what you stored.
+
+# As usual, always close them
+# shelfFile.close()
