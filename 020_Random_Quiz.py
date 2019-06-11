cd5ed634fd6b0a925eba7074d0a7a74669af512a
George Chan <ygchan89@gmail.com>
2019-06-11 10:17:33 -0400

    Update comment

--- 020_Random_Quiz.py
+++ 020_Random_Quiz.py
@@ -1,3 +1,11 @@
 # Credit: https://automatetheboringstuff.com/chapter8/
 # Project: Generating a Random Quiz and their files
 
+# 1. Create 35 different quizes
+# 2. Create 50 multiple choice quizes
+# 3. Provide the correct answer and 3 random wrong answer (random order)
+# 4. Write the quizzes to 35 text files
+# 5. Write the answer keys to 35 text files
+# 6. Store the states and their capitals in a dictionary
+# 7. open(), write(), close() for quiz and answer key text files
+# 8. Use random.shuffle() to randomize the order of the question and option.
