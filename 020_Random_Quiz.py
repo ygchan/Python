fc502ed257af9fee3b3acfa9d934297c2afccd1d
George Chan <ygchan89@gmail.com>
2019-06-12 21:03:19 -0400

    Continue project

--- 020_Random_Quiz.py
+++ 020_Random_Quiz.py
@@ -9,3 +9,34 @@
 # 6. Store the states and their capitals in a dictionary
 # 7. open(), write(), close() for quiz and answer key text files
 # 8. Use random.shuffle() to randomize the order of the question and option.
+
+import random
+
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+   'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
+   'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
+   'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois':
+   'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas':
+   'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine':
+   'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
+   'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
+   'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
+   'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico'
+    : 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
+   'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+   'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
+   'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
+   'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
+   'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia',
+   'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+
+for quizNum in range(5):
+    quizFile = open('capitalsquiz%s.txt' % (quizNum + 1), 'w')
+    answerKeyFile = open('capitalsquiz_answers%s.txt' % (quizNum + 1), 'w')
+
+    quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
+    quizFile.write((' ' * 20) + 'State Capital Quiz (Form %s)' % (quizNum + 1))
+    quizFile.write('\n\n')
+
+    states = list(capitals.keys())
+    random.shuffle(states)
