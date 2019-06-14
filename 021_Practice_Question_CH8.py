1d5d757d0c87e613dc529e91621c40a18391ee1e
George Chan <ygchan89@gmail.com>
2019-06-13 20:29:59 -0400

    Add a file

---
+++ 021_Practice_Question_CH8.py
@@ -0,0 +1,6 @@
+# Credit: https://automatetheboringstuff.com/chapter8/
+# Practice Questions on the back of the chapter
+
+# Question 01: What is a relative path relative to?
+# Relative path is related to the directory where the folder resides in.
+
