f696a14c5694ad3ad50e1380f47407d01efbe906
George Chan <ygchan89@gmail.com>
2019-06-26 17:21:40 -0400

    Update

--- 029_Rename_Filename_Date_Format.py
+++ 029_Rename_Filename_Date_Format.py
@@ -1,3 +1,37 @@
 # https://automatetheboringstuff.com/chapter9/
-# Rename the date styles!
+# Project rename files with American style dates to Eurpoean style dates
 
+# American style = MM-DD-YYYY
+# European style = DD-MM-YYYY
+
+# Write program that will handle it for you :)
+
+# Step 01: Search all the filesname in current working directory for AM style
+# Step 02: When you found it, swap it to European style.
+
+# Create a regex that can identify the text pattern for AM style dates
+# Call os.listdir() to find all the files
+# Loop over each filename, using the regex to check weather it has a date
+# If it has a date, rename the file with shutil.move()
+
+import shutil, os, re
+
+# Create a regex that matches files with the American date format.
+datePatter = re.compile(r"""^(.*?) # all text before the date
+    ((0|1)?\d)-                    # one or two digit for the month
+    ((0|1|2|3)?\d)-                # one or two digit for the day
+    ((19|20)\d\d)                  # four digits for the year
+    (.*?)$                         # all text after the date
+    """, re.VERBOSE)
+
+# To do: Loop over the files in the working directory
+
+# To do: skip files without a date
+
+# To do: Get the different parts of a filename
+
+# To do: From the European-style filename
+
+# To do: Get the full, absolute file paths
+
+# To do: Rename the files
