7bdc197a15ff6837f93f61f7e8969733cf60939a
George Chan <ygchan89@gmail.com>
2019-07-11 19:58:05 -0400

    Update

--- 031_Backup_Zip.py
+++ 031_Backup_Zip.py
@@ -1,1 +1,15 @@
 # Backup Program
+# Copies an entire folder and its contents into
+# a zip file whose filename will increments
+
+import zipfile, os
+
+def backupToZio(folder):
+  folder = os.path.abspath(folder)
+  
+  number = 1
+  while True:
+    zipFile = os.path.basename(folder) + '_' + str(number) + '.zip'
+    if not os.path.exits(zipFilename):
+      break
+     number = number + 1
