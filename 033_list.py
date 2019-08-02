fad064144de0e6af71696f2114ce3100056d7e99
George Chan <ygchan89@gmail.com>
2019-08-01 22:27:08 -0400

    Add a file

--- 033_list.py
+++ 033_list.py
@@ -3,3 +3,8 @@
 a = ['Python', 'is', 'a', 'great', 'programming', 'lanuage']
 for i in range(len(a)):
   print(i, a[i])
+  
+# Passing a number of strings to a file
+# where file is an file object
+def write_to_file_many_item(file, separator, *args):
+  file.write(separator.join(args))
