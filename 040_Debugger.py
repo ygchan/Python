598f50db39af9b0675ad1154c136a7557a177d0c
ygchan <ygchan89@gmail.com>
2019-08-02 21:52:12 -0400

    Add a file

---
+++ 040_Debugger.py
@@ -0,0 +1,16 @@
+# Chapter 10: Debugging
+# https://automatetheboringstuff.com/chapter10/
+
+# Using the IDE Debug Control Window to (Step)
+# and monitor each variable's value
+
+print('Enter the first number to add: ')
+first_num = input()
+
+print('Enter the second number to add: ')
+second_num = input()
+
+print('Enter the third number to add: ')
+third_num = input()
+
+print('The sum is ' + first_num + second_num + third_num)
