cb97a35c510f88dd8e798e3d99a59f739d0359de
ygchan <ygchan89@gmail.com>
2019-08-02 21:15:31 -0400

    Add a file

---
+++ 037_Traffic_Light_Simulation.py
@@ -0,0 +1,23 @@
+# Chapter 10: Debugging
+# https://automatetheboringstuff.com/chapter10/
+
+market_2nd = {'ns': 'green', 'ew': 'red'}
+missing_16th = {'ns': 'red', 'ew': 'green'}
+
+def switchLights(stoplight):
+    '''Takes an intersection dictionary as an arugment and switch the light'''
+
+    # If you want to make sure (assert) one of the light is always red
+    assert 'red' in stoplight.values(), 'Neight light is red!' + str(stoplight)
+
+    for key in stoplight.keys():
+        if stoplight[key] == 'green':
+            stoplight[key] == 'yellow'
+        elif stoplight[key] == 'yellow':
+            stoplight[key] == 'red'
+        elif stoplight[key] == 'red':
+            stoplight[key] = 'green'
+
+switchLights(market_2nd)
+
+# AssertionError: << Something bad happened.
