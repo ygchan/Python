444eae4eefeb86057054044707773fc67a960359
George Chan <ygchan89@gmail.com>
2019-10-27 22:17:40 -0400

    Learned: connect, cursor, fetchall

--- 000_DataQuest.py
+++ 000_DataQuest.py
@@ -1,5 +1,23 @@
+# Lesson 1: Connect()
 import psycopg2
 
 # Database name dq with user with the same name
 conn = psycopg2.connect("dbname=dq user=dq")
 conn.close()
+# -----------------------------------------------------
+
+# Lesson 2: Cursor()
+import psycopg2
+
+conn = psycopg2.connect("dbname=dq user=dq")
+cur = conn.cursor()
+cur.execute("select * from users;")
+
+# Fetch all rows result into a list
+all_results = cur.fetchall()
+
+conn.close()
+
+for result in all_results:
+    print(result)
+# -----------------------------------------------------
