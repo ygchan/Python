4345cd896b439638f3fd4ca4e41a69e1cb0de5bc
ygchan <ygchan89@gmail.com>
2020-02-29 20:54:23 -0500

    Add a file

---
+++ 000_LinkedIn_Learning_Python.py
@@ -0,0 +1,7 @@
+# Chatper 01: Setup
+# Download sample files
+# Check version using
+python 3 --version
+
+# Run certificate command for web access later on
+# Download Visual Studio from https://code.visualstudio.com/
