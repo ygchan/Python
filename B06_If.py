3a2978c3c0474f60835790fd3557f1cd2d8706bf
ygchan <ygchan89@gmail.com>
2020-03-10 22:33:29 -0400

    Add a file

---
+++ B06_If.py
@@ -0,0 +1,9 @@
+x = 42
+y = 73
+
+if x < y:
+	print('x < y: x is {} and y is {}'.format(x,y))
+
+# a lot of tips about indent
+# how to correctly indent your python code.
+# blocks is defined by indent
