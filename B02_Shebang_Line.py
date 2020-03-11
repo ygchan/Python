7a5e75083dc9d4385b4871b52a60a32a23a5a4f7
ygchan <ygchan89@gmail.com>
2020-03-10 22:12:26 -0400

    Learned: shebang line

---
+++ B02_Shebang_Line.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# this is shebang line - must be no line before this line and no spaces before
+# first 2 bytes and executable to the python.
+
+import platform
+
+# This is python verion 3.7.4
+print('This is python verion {}'.format(platform.python_version()))
