6e0a1b5b712473b7c85cf804d02fc1bc4e4b2e2d
ygchan <ygchan89@gmail.com>
2020-03-10 22:07:24 -0400

    Unable to setup Komodo IDE, crashed 4 times

---
+++ B01_Hello_World.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+print("Hello, World.")
+
+# Spend over 1 hour dealing with Komodo.
+# It is really complicated and crashed 4 times in a row
+# Ended up using SubLime instead.
