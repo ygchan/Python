c837825291b4dab69fec8bed6846408943d6b577
ygchan <ygchan89@gmail.com>
2020-03-10 22:47:28 -0400

    Learned: for loop

---
+++ B09_For.py
@@ -0,0 +1,8 @@
+words = ['one', 'two', 'three', 'four', 'five']
+
+# very use in the case of container
+# for <item> in items
+for word in words:
+	print(word)
+
+# author seems to like for loop a lot more
