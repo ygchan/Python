355241480e2de13a2a5e1464f676e69779a4aab9
ygchan <ygchan89@gmail.com>
2020-03-10 22:16:57 -0400

    Learned: statement vs. expression

---
+++ B03_Expression.py
@@ -0,0 +1,10 @@
+# Statment = unit of execution
+# Statment is a line of code, does not need semi-colon at the end.
+# Statement does not have to return a value.
+# Usually you write one statment per line.
+
+print("Hello") ; print("World") # Extremely rare.
+
+# Expression = unit of evaluation
+# Any combination of value / operation
+# Expression can be think of anything return a value or values.
